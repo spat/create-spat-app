c14b073bf5d5dc7e6a4e33ff789bdf0371845190
phi <phi1618jp@gmail.com>
2021-03-06 17:45:22 +0900

    download spat-template

--- cli.js
+++ cli.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
 console.log('Hello, spat!');
+
+var ghdownload = require('github-download');
+
+var dir = process.argv[2];
+
+if (dir) {
+  ghdownload({ user: 'spat', repo: 'spat-template', ref: 'develop'}, dir)
+    .on('end', () => {
+      console.log('finish!');
+    });
+}
+else {
+  console.log('ディレクトリ名を指定してください');
+}
